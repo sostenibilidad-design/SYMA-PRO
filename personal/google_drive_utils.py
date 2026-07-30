742f01788470cc1ee16820f0346d63beb858f904
Anyi Lorena Vasquez Sanchez <sostenibilidad@syma.com.co>
2026-07-30 15:21:23 -0500

    Add blank lines for improved readability

--- personal/google_drive_utils.py
+++ personal/google_drive_utils.py
@@ -19,6 +19,8 @@ from googleapiclient.http import MediaIoBaseDownload
 from .models import Empleado
 from core.sincronizacion_actividades import procesar_actividades_excel
 
+
+
 # CONFIGURACIÓN
 SCOPES = ["https://www.googleapis.com/auth/drive.readonly"]
 
